3e90f70aaec505020347cdd64353b5c2ddaa9a63
Taejin Kim <rlaxowls1316@naver.com>
2023-06-28 14:42:15 +0900

    Create an invader

--- index.js
+++ index.js
@@ -57,6 +57,47 @@ class Player {
   }
 }
 
+class Invader {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+
+    const image = new Image();
+    image.src = "./assets/invader.png";
+
+    image.onload = () => {
+      const scale = 1;
+      this.image = image;
+      this.width = image.width * scale;
+      this.height = image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2,
+      };
+    };
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+  }
+}
+
 class Projectile {
   constructor({ position, velocity }) {
     this.position = position;
@@ -82,6 +123,7 @@ class Projectile {
 
 const player = new Player();
 const projectiles = [];
+const invader = new Invader();
 
 const keys = {
   a: {
@@ -100,6 +142,8 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  invader.update();
+
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y <= 0) {
       projectiles.splice(index, 1);

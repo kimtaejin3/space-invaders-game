9969611a132bdb64b07668c03cf42397c8bd9edc
Taejin Kim <rlaxowls1316@naver.com>
2023-06-29 14:57:02 +0900

    Create and move grids

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ class Player {
 }
 
 class Invader {
-  constructor() {
+  constructor({ position }) {
     this.velocity = {
       x: 0,
       y: 0,
@@ -68,13 +68,13 @@ class Invader {
     image.src = "./assets/invader.png";
 
     image.onload = () => {
-      const scale = 1;
+      const scale = 0.8;
       this.image = image;
       this.width = image.width * scale;
       this.height = image.height * scale;
       this.position = {
-        x: canvas.width / 2 - this.width / 2,
-        y: canvas.height / 2,
+        x: position.x,
+        y: position.y,
       };
     };
   }
@@ -89,11 +89,56 @@ class Invader {
     );
   }
 
-  update() {
+  update({ velocity }) {
     if (this.image) {
       this.draw();
-      this.position.x += this.velocity.x;
-      this.position.y += this.velocity.y;
+      this.position.x += velocity.x;
+      this.position.y += velocity.y;
+    }
+  }
+}
+
+class Grid {
+  constructor() {
+    this.position = {
+      x: 0,
+      y: 0,
+    };
+
+    this.velocity = {
+      x: 3,
+      y: 0,
+    };
+
+    this.invaders = [];
+
+    const columns = Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 5 + 2);
+
+    this.width = columns * 30;
+
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        this.invaders.push(
+          new Invader({
+            position: {
+              x: x * 30,
+              y: y * 30,
+            },
+          })
+        );
+      }
+    }
+  }
+
+  update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y = 0;
+
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.y = 30;
     }
   }
 }
@@ -108,7 +153,7 @@ class Projectile {
 
   draw() {
     c.beginPath();
-    c.fillStyle = "red";
+    c.fillStyle = "lightyellow";
     c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
     c.fill();
     c.closePath();
@@ -123,7 +168,7 @@ class Projectile {
 
 const player = new Player();
 const projectiles = [];
-const invader = new Invader();
+const grids = [new Grid()];
 
 const keys = {
   a: {
@@ -142,13 +187,19 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
-  invader.update();
 
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y <= 0) {
       projectiles.splice(index, 1);
     }
     projectile.update();
+  });
+
+  grids.forEach((grid) => {
+    grid.update();
+    grid.invaders.forEach((invader) => {
+      invader.update({ velocity: grid.velocity });
+    });
   });
 
   if (keys.a.pressed && player.position.x >= 0) {

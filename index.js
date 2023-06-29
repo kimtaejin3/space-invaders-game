e2cc4c966545ac8cff254f218cb1c79d3da88e95
Taejin Kim <rlaxowls1316@naver.com>
2023-06-29 15:17:12 +0900

    Spawn grids at intervals

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ class Projectile {
 
 const player = new Player();
 const projectiles = [];
-const grids = [new Grid()];
+const grids = [];
 
 const keys = {
   a: {
@@ -182,6 +182,8 @@ const keys = {
   },
 };
 
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 500 + 500);
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
@@ -215,6 +217,13 @@ function animate() {
     player.velocity.x = 0;
     player.rotation = 0;
   }
+
+  if (frames % randomInterval === 0) {
+    grids.push(new Grid());
+    randomInterval = Math.floor(Math.random() * 500 + 500);
+  }
+
+  frames++;
 }
 
 animate();
